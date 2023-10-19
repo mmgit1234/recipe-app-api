fec725668d72a4595630da4a19b412716952cea5
Mark Mathews <markmathews@Marks-Laptop.local>
2023-10-18 21:04:20 -0500

    CC commit

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -106,7 +106,7 @@ class PrivateTagsApiTests(TestCase):
         )
         recipe.tags.add(tag1)
 
-        res = self.client.get(TAGS_URL, {'assinged_only': 1})
+        res = self.client.get(TAGS_URL, {'assigned_only': 1})
 
         s1 = TagSerializer(tag1)
         s2 = TagSerializer(tag2)
